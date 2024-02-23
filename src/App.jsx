8ef85dd68c5dca0f206988eeaef40e2c5355ef14
rolando-d3v <landon3388@gmail.com>
2024-02-22 20:48:55 -0500

    re

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import css from "./app.module.scss";
 import Iconos from "./components/iconos/Iconos";
 import IconosAnimados from "./components/iconos_animados/IconosAnimados";
 import Frame from "./components/figma/Framer";
-import Prueba from "./components/prueba/Prueba";
+
 
 function App() {
   return (
     <div className={css.app}>
       <Iconos />
       <IconosAnimados />
-      <Frame />
-      <Prueba />
+      {/* <Frame /> */}
+     
     </div>
   );
 }
